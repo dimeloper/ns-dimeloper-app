8f3e5d433ac320197f2dca426933249201261e11
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2019-08-31 23:20:43 +0000

    Experimenting with the AbsoluteLayout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StackComponent } from './layouts/stack/stack.component';
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { FlexboxComponent } from './layouts/flexbox/flexbox.component';
 import { GridComponent } from './layouts/grid/grid.component';
+import { AbsoluteComponent } from './layouts/absolute/absolute.component';
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
@@ -26,7 +27,8 @@ import { GridComponent } from './layouts/grid/grid.component';
         CurrentChallengeComponent,
         StackComponent,
         FlexboxComponent,
-        GridComponent
+        GridComponent,
+        AbsoluteComponent
     ],
     providers: [],
     schemas: [
