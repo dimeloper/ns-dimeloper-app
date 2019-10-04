a4ee7cb1645bad5524a55b1d79c3098923498297
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2019-10-04 16:48:45 +0000

    Interactive today view, included first CSS animation.

--- src/app/challenges/challenge-actions/challenge-actions.component.ts
+++ src/app/challenges/challenge-actions/challenge-actions.component.ts
@@ -10,12 +10,18 @@ export class ChallengeActionsComponent {
   @Input() cancelText = 'Cancel';
   @Output() actionSelected = new EventEmitter<DayStatus>();
 
+  action: 'complete' | 'fail' = null;
+
   onAction(action: 'complete' | 'fail' | 'cancel') {
     let status = DayStatus.Open;
     if (action === 'complete') {
       status = DayStatus.Completed;
+      this.action = 'complete';
     } else if (action === 'fail') {
-      status = DayStatus.Failed
+      status = DayStatus.Failed;
+      this.action = 'fail';
+    } else if (action === 'cancel') {
+      this.action = null;
     }
 
     this.actionSelected.emit(status);
--- src/app/challenges/challenge-actions/challenge-actions.module.ts
+++ src/app/challenges/challenge-actions/challenge-actions.module.ts
@@ -1,7 +1,9 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ChallengeActionsComponent } from '~/app/challenges/challenge-actions/challenge-actions.component';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
 @NgModule({
+  imports: [NativeScriptCommonModule],
   exports: [ChallengeActionsComponent],
   declarations: [ChallengeActionsComponent],
   schemas: [NO_ERRORS_SCHEMA]
